b01dc76a31b3b6fbc2b3b6ae94d97ad521c1a49f
geothermal-1408 <savvy3526@gmail.com>
2026-09-23 13:03:28 +0530

    use variable port no

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,17 @@
 import type { NextConfig } from "next";
+import dotenv from "dotenv";
 
+const port = process.env.BACKEND_PORT || 5000;
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*",
+        destination: `http://localhost:${port}/api/:path*`,
       },
       {
         source: "/uploads/:path*",
-        destination: "http://localhost:5000/uploads/:path*",
+        destination: `http://localhost:${port}/uploads/:path*`,
       },
     ];
   },
